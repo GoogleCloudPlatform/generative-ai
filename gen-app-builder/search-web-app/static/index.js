401df3220d24516eac92f5b4539a76cd7d757814
Holt Skinner <holtskinner@google.com>
2023-06-21 13:49:58 -0500

    Updates for linting

--- gen-app-builder/search-web-app/static/index.js
+++ gen-app-builder/search-web-app/static/index.js
@@ -3,5 +3,7 @@ mdc.autoInit();
 const MDCTabBar = mdc.tabBar.MDCTabBar;
 const MDCTextField = mdc.textField.MDCTextField;
 
-const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
-const queryTextField = new MDCTextField(document.querySelector('.mdc-text-field.query-field'));
+const tabBar = new MDCTabBar(document.querySelector(".mdc-tab-bar"));
+const queryTextField = new MDCTextField(
+  document.querySelector(".mdc-text-field.query-field")
+);
