8b5daaed519ced84bb98de5959ecb25fd11b9b49
Holt Skinner <holtskinner@google.com>
2023-06-22 11:25:00 -0500

    Updates to widget display and custom UI based on product changes

--- gen-app-builder/search-web-app/genappbuilder_utils.py
+++ gen-app-builder/search-web-app/genappbuilder_utils.py
@@ -15,7 +15,7 @@
 """Generative AI App Builder Utilities"""
 from typing import List, Tuple
 
-from google.cloud import discoveryengine_v1 as genappbuilder
+from google.cloud import discoveryengine
 
 JSON_INDENT = 2
 
@@ -27,7 +27,7 @@ def search_enterprise_search(
     search_query: str,
 ) -> Tuple:
     # Create a client
-    client = genappbuilder.SearchServiceClient()
+    client = discoveryengine.SearchServiceClient()
 
     # The full resource name of the search engine serving config
     # e.g. projects/{project_id}/locations/{location}
@@ -38,12 +38,12 @@ def search_enterprise_search(
         serving_config="default_config",
     )
 
-    request = genappbuilder.SearchRequest(
+    request = discoveryengine.SearchRequest(
         serving_config=serving_config, query=search_query, page_size=50
     )
     response_pager = client.search(request)
 
-    response = genappbuilder.SearchResponse(
+    response = discoveryengine.SearchResponse(
         results=response_pager.results,
         facets=response_pager.facets,
         guided_search_result=response_pager.guided_search_result,
@@ -54,14 +54,12 @@ def search_enterprise_search(
         summary=response_pager.summary,
     )
 
-    request_url = (
-        f"https://discoveryengine.googleapis.com/v1beta/{serving_config}:search"
-    )
+    request_url = f"https://discoveryengine.googleapis.com/v1/{serving_config}:search"
 
-    request_json = genappbuilder.SearchRequest.to_json(
+    request_json = discoveryengine.SearchRequest.to_json(
         request, including_default_value_fields=True, indent=JSON_INDENT
     )
-    response_json = genappbuilder.SearchResponse.to_json(
+    response_json = discoveryengine.SearchResponse.to_json(
         response, including_default_value_fields=False, indent=JSON_INDENT
     )
 
@@ -69,7 +67,7 @@ def search_enterprise_search(
     return results, request_url, request_json, response_json
 
 
-def get_results(response: genappbuilder.SearchResponse) -> List:
+def get_results(response: discoveryengine.SearchResponse) -> List:
     """
     Extract Results from Enterprise Search Response
     """
@@ -86,7 +84,7 @@ def get_results(response: genappbuilder.SearchResponse) -> List:
                 "displayLink": data["displayLink"],
                 "snippets": [s["htmlSnippet"] for s in data["snippets"]],
                 "thumbnailImage": data["pagemap"]["cse_thumbnail"][0]["src"],
-                "resultJson": genappbuilder.SearchResponse.SearchResult.to_json(
+                "resultJson": discoveryengine.SearchResponse.SearchResult.to_json(
                     result, including_default_value_fields=True, indent=JSON_INDENT
                 ),
             }
