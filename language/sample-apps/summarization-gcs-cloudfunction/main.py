5b8fd4885377c7ac37bf11287b3c0c5329044a74
Romin Irani <1614870+rominirani@users.noreply.github.com>
2024-01-06 00:45:14 +0530

    upgrade to newer versions of PaLM models (#326)
    Co-authored-by: Holt Skinner <holtskinner@google.com>
    Co-authored-by: Holt Skinner <13262395+holtskinner@users.noreply.github.com>

--- language/sample-apps/summarization-gcs-cloudfunction/main.py
+++ language/sample-apps/summarization-gcs-cloudfunction/main.py
@@ -7,8 +7,8 @@ from vertexai.language_models import TextGenerationModel
 import google.cloud.logging
 
 
-PROJECT_ID  = os.environ.get('GCP_PROJECT','-')
-LOCATION = os.environ.get('GCP_REGION','-')
+PROJECT_ID = os.environ.get("GCP_PROJECT", "-")
+LOCATION = os.environ.get("GCP_REGION", "-")
 
 client = google.cloud.logging.Client(project=PROJECT_ID)
 client.setup_logging()
@@ -16,11 +16,13 @@ client.setup_logging()
 LOG_NAME = "summarize-cloudfunction-log"
 logger = client.logger(LOG_NAME)
 
+
 def predict_text(prompt, **parameters):
     vertexai.init(project=PROJECT_ID, location=LOCATION)
-    model = TextGenerationModel.from_pretrained("text-bison@001")
-    prompt_response = model.predict(prompt,**parameters)
+    model = TextGenerationModel.from_pretrained("text-bison@002")
+    prompt_response = model.predict(prompt, **parameters)
     return prompt_response.text
+
 
 @functions_framework.cloud_event
 def summarize_gcs_object(cloud_event):
@@ -39,16 +41,16 @@ def summarize_gcs_object(cloud_event):
     # Invoke the predict function with the Summarize prompt
     prompt = f"Summarize the following: {file_contents}"
     parameters = {
-            "temperature": 0.2,
-            "max_output_tokens": 256,
-            "top_p": 0.8,
-            "top_k": 40
-        }
-    prompt_response = predict_text(prompt,**parameters)
+        "temperature": 0.2,
+        "max_output_tokens": 256,
+        "top_p": 0.8,
+        "top_k": 40,
+    }
+    prompt_response = predict_text(prompt, **parameters)
 
     # Save the summary in another blob (same name as the original blob) in the summary bucket
     summary_blob_name = name
     summarization_bucket = storage_client.bucket(f"{bucketname}-summaries")
     summary_blob = summarization_bucket.blob(summary_blob_name)
-    summary_blob.upload_from_string(prompt_response.encode('utf-8'))
+    summary_blob.upload_from_string(prompt_response.encode("utf-8"))
     logger.log(f"Summarization saved in {summary_blob_name} in {bucketname}-summaries.")
