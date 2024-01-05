d6572fcc7d5d602a96f87fe1754fae9c15b058e8
Romin Irani <1614870+rominirani@users.noreply.github.com>
2024-01-06 00:45:14 +0530

    upgrade to newer versions of PaLM models (#326)
    Co-authored-by: Holt Skinner <holtskinner@google.com>
    Co-authored-by: Holt Skinner <13262395+holtskinner@users.noreply.github.com>

--- language/sample-apps/chat-gradio/app.py
+++ language/sample-apps/chat-gradio/app.py
@@ -6,8 +6,8 @@ from vertexai.language_models import TextGenerationModel
 
 import gradio as gr
 
-PROJECT_ID = os.environ.get('GCP_PROJECT') #Your Google Cloud Project ID
-LOCATION = os.environ.get('GCP_REGION')   #Your Google Cloud Project Region
+PROJECT_ID = os.environ.get("GCP_PROJECT")  # Your Google Cloud Project ID
+LOCATION = os.environ.get("GCP_REGION")  # Your Google Cloud Project Region
 
 client = google.cloud.logging.Client(project=PROJECT_ID)
 client.setup_logging()
@@ -17,17 +17,20 @@ logger = client.logger(log_name)
 
 vertexai.init(project=PROJECT_ID, location=LOCATION)
 
-model = TextGenerationModel.from_pretrained("text-bison@001")
+model = TextGenerationModel.from_pretrained("text-bison@002")
+
 
 def predict(prompt, max_output_tokens, temperature, top_p, top_k):
     logger.log_text(prompt)
     answer = model.predict(
         prompt,
-        max_output_tokens=max_output_tokens, # default 128
-        temperature=temperature, # default 0
-        top_p=top_p, # default 1
-        top_k=top_k) # default 40
-    return answer
+        max_output_tokens=max_output_tokens,  # default 128
+        temperature=temperature,  # default 0
+        top_p=top_p,  # default 1
+        top_k=top_k,
+    )  # default 40
+    return answer.text
+
 
 examples = [
     ["What are some generative AI services on Google Cloud in Public Preview?"],
@@ -37,14 +40,18 @@ examples = [
 
 demo = gr.Interface(
     predict,
-    [ gr.Textbox(label="Enter prompt:", value="What are some generative AI services on Google Cloud in Public Preview?"),
-      gr.Slider(32, 1024, value=512, step = 32, label = "max_output_tokens"),
-      gr.Slider(0, 1, value=0.2, step = 0.1, label = "temperature"),
-      gr.Slider(0, 1, value=0.8, step = 0.1, label = "top_p"),
-      gr.Slider(1, 40, value=38, step = 1, label = "top_k"),
+    [
+        gr.Textbox(
+            label="Enter prompt:",
+            value="What are some generative AI services on Google Cloud in Public Preview?",
+        ),
+        gr.Slider(32, 1024, value=512, step=32, label="max_output_tokens"),
+        gr.Slider(0, 1, value=0.2, step=0.1, label="temperature"),
+        gr.Slider(0, 1, value=0.8, step=0.1, label="top_p"),
+        gr.Slider(1, 40, value=38, step=1, label="top_k"),
     ],
     "text",
-    examples=examples
-    )
+    examples=examples,
+)
 
 demo.launch(server_name="0.0.0.0", server_port=8080)
--- language/sample-apps/text-predict-cloudfunction/main.py
+++ language/sample-apps/text-predict-cloudfunction/main.py
@@ -7,8 +7,8 @@ import google.cloud.logging
 import vertexai
 from vertexai.language_models import TextGenerationModel
 
-PROJECT_ID  = os.environ.get('GCP_PROJECT','-')
-LOCATION = os.environ.get('GCP_REGION','-')
+PROJECT_ID = os.environ.get("GCP_PROJECT", "-")
+LOCATION = os.environ.get("GCP_REGION", "-")
 
 client = google.cloud.logging.Client(project=PROJECT_ID)
 client.setup_logging()
@@ -16,25 +16,25 @@ client.setup_logging()
 log_name = "predictText-cloudfunction-log"
 logger = client.logger(log_name)
 
+
 @functions_framework.http
 def predictText(request):
-
     request_json = request.get_json(silent=True)
 
-    if request_json and 'prompt' in request_json:
-        prompt = request_json['prompt']
+    if request_json and "prompt" in request_json:
+        prompt = request_json["prompt"]
         logger.log(f"Received request for prompt: {prompt}")
         vertexai.init(project=PROJECT_ID, location=LOCATION)
-        model = TextGenerationModel.from_pretrained("text-bison@001")
+        model = TextGenerationModel.from_pretrained("text-bison@002")
         parameters = {
             "temperature": 0.2,
             "max_output_tokens": 256,
             "top_p": 0.8,
-            "top_k": 40
+            "top_k": 40,
         }
-        prompt_response = model.predict(prompt,**parameters)
+        prompt_response = model.predict(prompt, **parameters)
         logger.log("PaLM Text Bison Model response: {prompt_response.text}")
     else:
-        prompt_response = 'No prompt provided.'
+        prompt_response = "No prompt provided."
 
-    return json.dumps({"response_text":prompt_response.text})
+    return json.dumps({"response_text": prompt_response.text})
