2bfa68b55ef01ac18aa6799cee69450f70b21e5b
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-11 12:43:29 -0500

    Fixing query error caused by linebreak

--- gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/image/query_remote_function.sql
+++ gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/image/query_remote_function.sql
@@ -14,7 +14,6 @@
 
 SELECT
   uri AS image_input,
-  `${project_id}.${dataset_id}.${bq_function_name}`(uri)
-    AS image_description
+  `${project_id}.${dataset_id}.${bq_function_name}`(uri) AS image_description
 FROM
   `${project_id}.${dataset_id}.${object_table_id}`; -- noqa: RF05
