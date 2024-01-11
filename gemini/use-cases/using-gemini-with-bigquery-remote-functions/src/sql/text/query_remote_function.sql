36e09c66c816fa6fc7985ac69d112db212204112
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-11 13:00:09 -0500

    error testing

--- gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/text/query_remote_function.sql
+++ gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/text/query_remote_function.sql
@@ -14,8 +14,6 @@
 
 SELECT
   text_prompt AS text_input,
-  `${project_id}.${dataset_id}.${bq_function_name}`(text_prompt)
-    AS landmark_description
+  `${project_id}.${dataset_id}.${bq_function_name}`(text_prompt) AS landmark_description
 FROM
-  `${project_id}.${dataset_id}.${sample_prompt_table}`
-; -- noqa: RF05
+  `${project_id}.${dataset_id}.${sample_prompt_table}`; -- noqa: RF05
