3adcf0df813ceac474a4efe9bbd24aea1652abd0
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-11 13:02:55 -0500

    error testing

--- gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/image/query_remote_function.sql
+++ gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/image/query_remote_function.sql
@@ -16,4 +16,5 @@ SELECT
   uri AS image_input,
   `${project_id}.${dataset_id}.${bq_function_name}` (uri) AS image_description
 FROM
-  `${project_id}.${dataset_id}.${object_table_id}`; -- noqa: RF05
+  `${project_id}.${dataset_id}.${object_table_id}` -- noqa: RF05
+;
