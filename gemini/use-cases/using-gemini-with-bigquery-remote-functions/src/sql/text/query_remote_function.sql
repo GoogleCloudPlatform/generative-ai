beebe519406758feb7244ac74a8b8f54e2287a14
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-11 14:21:03 -0500

    Resolve spacing

--- gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/text/query_remote_function.sql
+++ gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/text/query_remote_function.sql
@@ -15,7 +15,7 @@
 SELECT
   text_prompt AS text_input,
   `${project_id}.${dataset_id}.${bq_function_name}`(text_prompt)
-     AS landmark_description
+    AS landmark_description
 FROM
   `${project_id}.${dataset_id}.${sample_prompt_table}` -- noqa: RF05
 ;
