9734aa7c05fdf88121f08b45b7f8f79ed0dee594
Holt Skinner <holtskinner@google.com>
2024-01-11 11:08:46 -0600

    Updated SQL Linter

--- gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/image/provision_remote_function.sql
+++ gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/image/provision_remote_function.sql
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+-- noqa: disable=PRS
 CREATE OR REPLACE FUNCTION
   `${project_id}.${dataset_id}.${bq_function_name}`(gcs_uri STRING) RETURNS STRING
   REMOTE WITH CONNECTION `${project_id}.${region}.${bq_connection_id}`
--- gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/image/query_remote_function.sql
+++ gemini/use-cases/using-gemini-with-bigquery-remote-functions/src/sql/image/query_remote_function.sql
@@ -17,4 +17,4 @@ SELECT
   `${project_id}.${dataset_id}.${bq_function_name}`(uri)
     AS image_description
 FROM
-  `${project_id}.${dataset_id}.${object_table_id}`;
+  `${project_id}.${dataset_id}.${object_table_id}`; -- noqa: RF05
