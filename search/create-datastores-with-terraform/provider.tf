3557a60c799208e6729754a6e4f4bcc451429100
agutta <anitagutta@gmail.com>
2024-02-18 20:33:16 -0600

    Update provider.tf

--- search/create-datastores-with-terraform/provider.tf
+++ search/create-datastores-with-terraform/provider.tf
@@ -14,6 +14,18 @@
  * limitations under the License.
  */
 
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.16.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.16.0"
+    }
+  }
+}
 
 provider "google" {
   project               = var.project_id
