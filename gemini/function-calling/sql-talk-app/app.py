44ec5d2fd6595d314ff3c4f461b72ddcdf777c80
Kristopher Overholt <koverholt@google.com>
2024-02-26 11:17:31 -0600

    fix: [Gemini function calling] Edit sample prompt for accuracy (#425)
    Changes sample prompt in the Gemini function calling SQL Talk app to
    `What percentage of orders are returned?` as a simpler query to check
    only the orders table.

--- gemini/function-calling/sql-talk-app/app.py
+++ gemini/function-calling/sql-talk-app/app.py
@@ -101,7 +101,7 @@ with st.expander("Sample prompts", expanded=True):
     st.write(
         """
         - What kind of information is in this database?
-        - What percentage of customers return their order?
+        - What percentage of orders are returned?
         - How is inventory distributed across our regional distribution centers?
         - Do customers typically place more than one order?
         - Which product categories have the highest profit margins?
