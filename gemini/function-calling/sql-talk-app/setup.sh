15b4e9f6e67498bea42ae50ac1fa07c3ae030f2f
Kristopher Overholt <koverholt@google.com>
2024-03-14 09:09:22 -0500

    Update

--- gemini/function-calling/sql-talk-app/setup.sh
+++ gemini/function-calling/sql-talk-app/setup.sh
@@ -11,7 +11,7 @@ bq mk \
   --data_source=cross_region_copy \
   --target_dataset=thelook_ecommerce \
   --display_name='SQL Talk Sample Data' \
-  --schedule_end_time=$(date -u -d '5 mins' +%Y-%m-%dT%H:%M:%SZ) \
+  --schedule_end_time="$(date -u -d '5 mins' +%Y-%m-%dT%H:%M:%SZ)" \
   --params='{
       "source_project_id":"bigquery-public-data",
       "source_dataset_id":"thelook_ecommerce",
