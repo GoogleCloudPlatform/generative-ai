433840d1835147578c225296a08f10d9654334ac
Megan O'Keefe <3137106+askmeegs@users.noreply.github.com>
2024-03-29 13:18:03 -0400

    feat: add Gemini sample app: "Fix My Car" [WIP] (#458)
    # Description
    
    Fix My Car is a generative AI sample application using Gemini, Vertex AI
    Search, Cloud SQL, and Streamlit/Java on GKE. It is an auto manual
    helper chatbot.
    
    There are two separate flavors of the app, packaged separately. One uses
    Vertex AI Search for grounding, and the other uses Cloud SQL with
    pgvector.
    
    Note: This app can use any PDF owner's manuals for grounding. For this
    sample version, we use a Gemini-generated manual for a mock car model
    called "Cymbal Starlight."
    
    ### checklist 
    
    - [x] Follow the [`CONTRIBUTING`
    Guide](https://github.com/GoogleCloudPlatform/generative-ai/blob/main/CONTRIBUTING.md).
    - [x] You are listed as the author in your notebook or README file.
    - [ ] Your account is listed in
    [`CODEOWNERS`](https://github.com/GoogleCloudPlatform/generative-ai/blob/main/.github/CODEOWNERS)
    for the file(s).
    - [ ] Make your Pull Request title in the
    <https://www.conventionalcommits.org/> specification.
    - [ ] Ensure the tests and linter pass (Run `nox -s format` from the
    repository root to format).
    - [ ] Appropriate docs were updated (if necessary)
    
    ---------
    
    Co-authored-by: Holt Skinner <13262395+holtskinner@users.noreply.github.com>
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Holt Skinner <holtskinner@google.com>

---
+++ gemini/sample-apps/fixmycar/cloud-sql/backend/src/test/java/com/cpet/fixmycarbackend/FixMyCarBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cpet.fixmycarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixMyCarBackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
