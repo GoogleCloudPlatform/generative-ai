2fa057846807386384573dff2420508b51e3363e
cmelby <cmelby@google.com>
2024-04-18 17:03:47 +0000

    Updateing

--- gemini/function-calling/sql-talk-app/app.py
+++ gemini/function-calling/sql-talk-app/app.py
@@ -4,7 +4,7 @@ from google.cloud import bigquery
 import streamlit as st
 from vertexai.generative_models import FunctionDeclaration, GenerativeModel, Part, Tool
 
-BIGQUERY_DATASET_ID = "google.com:lps-global-sales-engine-243309"
+BIGQUERY_DATASET_ID = "dst-master.product_adoption.p0_totalStats"
 
 list_datasets_func = FunctionDeclaration(
     name="list_datasets",
