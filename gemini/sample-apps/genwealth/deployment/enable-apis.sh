adf77fbb56822dcede53fd5ac25139e32adeb3ba
Paul Ramsey <22893884+paulramsey@users.noreply.github.com>
2024-04-24 16:47:31 -0500

    fix: Combine API calls into batches.

--- gemini/sample-apps/genwealth/deployment/enable-apis.sh
+++ gemini/sample-apps/genwealth/deployment/enable-apis.sh
@@ -1,31 +1,29 @@
 #!/usr/bin/env bash
 
-# Enable Backend APIs
+# Enable first batch of APIs (20 max per batch)
 PROJECT_ID=$(gcloud config get-value project 2> /dev/null)
-gcloud services enable iam.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable compute.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable storage-component.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable pubsub.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable cloudkms.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable logging.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable alloydb.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable servicedirectory.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable serviceusage.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable networkmanagement.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable cloudresourcemanager.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable servicenetworking.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable dns.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable orgpolicy.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable aiplatform.googleapis.com --project "${PROJECT_ID}"
+gcloud services enable iam.googleapis.com \
+    compute.googleapis.com \
+    storage-component.googleapis.com \
+    pubsub.googleapis.com \
+    cloudkms.googleapis.com \
+    logging.googleapis.com \
+    alloydb.googleapis.com \
+    servicedirectory.googleapis.com  \
+    serviceusage.googleapis.com \
+    networkmanagement.googleapis.com \
+    cloudresourcemanager.googleapis.com \
+    servicenetworking.googleapis.com \
+    dns.googleapis.com \
+    orgpolicy.googleapis.com \
+    aiplatform.googleapis.com \
+    cloudfunctions.googleapis.com \
+    eventarc.googleapis.com \
+    secretmanager.googleapis.com \
+    vpcaccess.googleapis.com \
+    documentai.googleapis.com --project "${PROJECT_ID}"
 
-# Enable pipeline APIs
-gcloud services enable cloudfunctions.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable eventarc.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable secretmanager.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable vpcaccess.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable documentai.googleapis.com --project "${PROJECT_ID}"
-
-# Enable front end APIs
-gcloud services enable run.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable artifactregistry.googleapis.com --project "${PROJECT_ID}"
-gcloud services enable cloudbuild.googleapis.com --project "${PROJECT_ID}"
+# Enable second batch of APIs
+gcloud services enable run.googleapis.com \
+    artifactregistry.googleapis.com \
+    cloudbuild.googleapis.com --project "${PROJECT_ID}"
