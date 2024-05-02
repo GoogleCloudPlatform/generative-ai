dfeb02f5cf96c76e5acd8f2f9e327058bfc7854b
navyasingla <navyasingla@google.com>
2024-05-02 17:53:24 +0530

    linting

--- gemini/sample-apps/accelerating_product_innovation/app/pages_utils/utils_export_content_pdf.py
+++ gemini/sample-apps/accelerating_product_innovation/app/pages_utils/utils_export_content_pdf.py
@@ -5,7 +5,10 @@ layouts and formatting.
 
 import os
 
-from app.pages_utils.utils_pdf_generation import add_formatted_page, check_add_page
+from app.pages_utils.utils_pdf_generation import (
+    add_formatted_page,
+    check_add_page,
+)
 from app.pages_utils.utils_pdf_generation import PDFRounded as FPDF
 import streamlit as st
 
@@ -65,7 +68,9 @@ def create_pdf_layout(
             pdf.multi_cell(170, 5, page)  # Output the text
 
 
-def create_content_pdf(product_content: list, selected_titles: list[str]) -> None:
+def create_content_pdf(
+    product_content: list, selected_titles: list[str]
+) -> None:
     """Creates a PDF for each product content and selected title.
 
     Args:
@@ -80,7 +85,9 @@ def create_content_pdf(product_content: list, selected_titles: list[str]) -> Non
         images = []
 
         # Build content and image lists for the current product
-        content.append(product_content[product_index][0]["text"].replace("**", ""))
+        content.append(
+            product_content[product_index][0]["text"].replace("**", "")
+        )
         images.append(st.session_state.num_drafts * product_index + 1)
 
         # Generate the PDF layout
