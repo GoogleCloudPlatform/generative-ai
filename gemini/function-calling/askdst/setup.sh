4b6e334c8f2f06be93c1380931ba436ad476a18d
cmelby <cmelby@google.com>
2024-05-15 21:35:01 +0000

    updating fire store and dst app

--- gemini/function-calling/askdst/setup.sh
+++ gemini/function-calling/askdst/setup.sh
@@ -10,12 +10,11 @@ bq mk \
   --transfer_config \
   --data_source=cross_region_copy \
   --target_dataset=thelook_ecommerce \
-  --display_name='SQL Talk Sample Data' \
+  --display_name='askDST' \
   --schedule_end_time="$(date -u -d '5 mins' +%Y-%m-%dT%H:%M:%SZ)" \
   --params='{
-      "source_project_id":"bigquery-public-data",
-      "source_dataset_id":"thelook_ecommerce",
-      "overwrite_destination_table":"true"
+      "source_project_id":"dst-master",
+      "source_dataset_id":"dst_gpsell"
       }'
 
 # Install Python
