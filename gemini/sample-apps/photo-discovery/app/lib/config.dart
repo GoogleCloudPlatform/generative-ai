fdd5790be9cf23c236ceb5f2e9971e5d009d08d6
Khanh Nguyen <khanhnwin@gmail.com>
2024-06-13 13:55:14 -0700

    add lib directory containing project files

---
+++ gemini/sample-apps/photo-discovery/app/lib/config.dart
@@ -0,0 +1,2 @@
+const geminiModel = 'gemini-1.5-pro-preview-0409';
+const cloudRunHost = 'ag-web-nhhfh7g7iq-uc.a.run.app';
---
+++ gemini/sample-apps/photo-discovery/app/lib/functionality/adaptive/capabilities.dart
@@ -0,0 +1,24 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart';
+
+/* 
+This code assumes no camera access and always has physical keyboardfor demo 
+purposes. Please use native OS APIs to determine if the device has certain
+capabilities. See flutter.dev/adaptive for additional guidance.
+*/
+
+class Capabilities {
+  static bool get hasCamera {
+    return false;
+  }
+
+  static bool get hasPhysicalKeyboard {
+    return true;
+  }
+}
+
+// Utilities to determine Platform
+bool get isMobile => Platform.isAndroid || Platform.isIOS;
+bool get isDesktop =>
+    Platform.isLinux || Platform.isMacOS || Platform.isWindows;
+bool get isWeb => kIsWeb;
