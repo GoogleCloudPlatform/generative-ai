561786259d8978d1b21a544f4880d835a14a57d6
Holt Skinner <holtskinner@google.com>
2024-08-30 14:32:35 -0500

    Fix checker issues

--- .github/workflows/notebook_linter/check_links.py
+++ .github/workflows/notebook_linter/check_links.py
@@ -1,5 +1,8 @@
+"""Check links in notebooks for accuracy."""
+
 import os
 import sys
+from typing import Tuple
 import urllib.parse
 
 import nbformat
@@ -16,14 +19,16 @@ RAW_URL_PREFIX = (
 )
 
 
-def fix_markdown_links(cell_source, relative_notebook_path: str):
+def fix_markdown_links(
+    cell_source: str, relative_notebook_path: str
+) -> Tuple[str, bool]:
     """Fixes links in a markdown cell and returns the updated source."""
     new_lines = []
     changes_made = False
 
     for line in cell_source.splitlines():
         for key, prefix in LINK_PREFIXES.items():
-            if not prefix in line:
+            if prefix not in line:
                 continue
 
             start_index = line.find(prefix) + len(prefix)
@@ -33,16 +38,16 @@ def fix_markdown_links(cell_source, relative_notebook_path: str):
             if key in {"colab_link", "github_link"}:
                 correct_link = relative_notebook_path
             elif key == "colab_enterprise_link":
-                encoded_path = urllib.parse.quote(
+                correct_link = urllib.parse.quote(
                     f"{RAW_URL_PREFIX}{relative_notebook_path}",
                     safe=":",
                 )
-                correct_link = f"{prefix}{encoded_path}"
             elif key == "workbench_link":
-                correct_link = f"{prefix}{RAW_URL_PREFIX}{relative_notebook_path}"
+                correct_link = f"{RAW_URL_PREFIX}{relative_notebook_path}"
 
             if correct_link not in line:
-                print(f"Fixing {key} in {relative_notebook_path}: {line}")
+                print(f"Incorrect link in {relative_notebook_path}: {line}\n")
+                print(f"Should be: {correct_link}\n")
                 line = line.replace(line[start_index:end_index], correct_link)
                 changes_made = True
 
@@ -51,7 +56,7 @@ def fix_markdown_links(cell_source, relative_notebook_path: str):
     return "\n".join(new_lines), changes_made
 
 
-def fix_links_in_notebook(notebook_path: str):
+def fix_links_in_notebook(notebook_path: str) -> int:
     """Fixes specific types of links in a Jupyter notebook."""
     with open(notebook_path, "r", encoding="utf-8") as f:
         notebook = nbformat.read(f, as_version=4)
@@ -67,7 +72,6 @@ def fix_links_in_notebook(notebook_path: str):
                 cell.source = updated_source
                 with open(notebook_path, "w", encoding="utf-8") as f:
                     nbformat.write(notebook, f)
-                print(f"Updated {notebook_path}\n")
                 return 1
     return 0
 
