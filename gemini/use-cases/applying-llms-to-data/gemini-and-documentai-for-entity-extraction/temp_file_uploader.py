013b265a457b08afda83203186d299719e6d7935
Valentin Huerta <valentinhuerta@google.com>
2024-10-02 13:43:06 -0600

    fix:spelling mistakes

--- gemini/use-cases/applying-llms-to-data/gemini-and-documentai-for-entity-extraction/temp_file_uploader.py
+++ gemini/use-cases/applying-llms-to-data/gemini-and-documentai-for-entity-extraction/temp_file_uploader.py
@@ -26,9 +26,7 @@ class TempFileUploader:
         blob = bucket.blob(self.destination_blob_name)
         blob.upload_from_filename(file_path)
 
-        gcs_destination_uri = (
-            f"gs://{self.temp_bucket_name}/{self.destination_blob_name}"
-        )
+        gcs_destination_uri = f"gs://{self.temp_bucket_name}/{self.destination_blob_name}"
         return gcs_destination_uri
 
     def delete_file(self) -> None:
