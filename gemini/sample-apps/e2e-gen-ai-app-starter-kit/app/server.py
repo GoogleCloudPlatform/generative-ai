kramitsri/gcp

feat: add conversational genai app template

gemini/sample-apps/conversational-genai-app-template/app/server.py
@@ -1,0 +1,113 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import os
+from typing import AsyncGenerator
+import uuid
+
+# ruff: noqa: I001
+## Import the chain to be used
+from app.chain import chain
+from app.utils.input_types import Feedback, Input, InputChat, default_serialization
+from app.utils.output_types import EndEvent, Event
+from app.utils.tracing import CloudTraceLoggingSpanExporter
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse, StreamingResponse
+from google.cloud import logging as gcp_logging
+from traceloop.sdk import Instruments, Traceloop
+
+# Or choose one of the following pattern chains to test by uncommenting it:
+
+# Custom RAG QA
+# from app.patterns.custom_rag_qa.chain import chain
+
+# LangGraph dummy agent
+# from app.patterns.langgraph_dummy_agent.chain import chain
+
+# The events that are supported by the UI Frontend
+SUPPORTED_EVENTS = [
+    "on_tool_start",
+    "on_tool_end",
+    "on_retriever_start",
+    "on_retriever_end",
+    "on_chat_model_stream",
+]
+
+# Initialize FastAPI app and logging
+app = FastAPI()
+logging_client = gcp_logging.Client()
+logger = logging_client.logger(__name__)
+
+# Initialize Traceloop
+try:
+    Traceloop.init(
+        app_name="Sample Chatbot Application",
+        disable_batch=False,
+        exporter=CloudTraceLoggingSpanExporter(),
+        instruments={Instruments.VERTEXAI, Instruments.LANGCHAIN},
+    )
+except Exception as e:
+    logging.error("Failed to initialize Traceloop: %s", e)
+
+
+async def stream_event_response(input_chat: InputChat) -> AsyncGenerator[str, None]:
+    run_id = uuid.uuid4()
+    input_dict = input_chat.model_dump()
+    Traceloop.set_association_properties(
+        {
+            "log_type": "tracing",
+            "run_id": str(run_id),
+            "user_id": input_dict["user_id"],
+            "session_id": input_dict["session_id"],
+            "commit_sha": os.environ.get("COMMIT_SHA", "None"),
+        }
+    )
+
+    yield json.dumps(
+        Event(event="metadata", data={"run_id": str(run_id)}),
+        default=default_serialization,
+    ) + "\n"
+
+    async for data in chain.astream_events(input_dict, version="v2"):
+        if data["event"] in SUPPORTED_EVENTS:
+            yield json.dumps(data, default=default_serialization) + "\n"
+
+    yield json.dumps(EndEvent(), default=default_serialization) + "\n"
+
+
+# Routes
+@app.get("/")
+async def redirect_root_to_docs() -> RedirectResponse:
+    return RedirectResponse("/docs")
+
+
+@app.post("/feedback")
+async def collect_feedback(feedback_dict: Feedback) -> None:
+    logger.log_struct(feedback_dict.model_dump(), severity="INFO")
+
+
+@app.post("/stream_events")
+async def stream_chat_events(request: Input) -> StreamingResponse:
+    return StreamingResponse(
+        stream_event_response(input_chat=request.input), media_type="text/event-stream"
+    )
+
+
+# Main execution
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)

gemini/sample-apps/conversational-genai-app-template/deployment/terraform/apis.tf
@@ -1,0 +1,47 @@
+locals {
+  cicd_services = [
+    "cloudbuild.googleapis.com",
+    "discoveryengine.googleapis.com",
+    "aiplatform.googleapis.com",
+    "serviceusage.googleapis.com",
+    "bigquery.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+  ]
+  
+  shared_services = [
+    "aiplatform.googleapis.com",
+    "run.googleapis.com",
+    "discoveryengine.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "bigquery.googleapis.com",
+    "serviceusage.googleapis.com",
+    "logging.googleapis.com"
+  ]
+  
+  projects = {
+    prod    = var.prod_project_id
+    staging = var.staging_project_id
+  }
+
+}
+
+resource "google_project_service" "cicd_services" {
+  count   = length(local.cicd_services)
+  project = var.cicd_runner_project_id
+  service = local.cicd_services[count.index]
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "shared_services" {
+  for_each = {
+    for pair in setproduct(keys(local.projects), local.shared_services) : 
+    "${pair[0]}_${replace(pair[1], ".", "_")}" => {
+      project = local.projects[pair[0]]
+      service = pair[1]
+    }
+  }
+  project            = each.value.project
+  service            = each.value.service
+  disable_on_destroy = false
+}

gemini/sample-apps/conversational-genai-app-template/streamlit/side_bar.py
@@ -1,0 +1,160 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import uuid
+
+from utils.multimodal_utils import (
+    HELP_GCS_CHECKBOX,
+    HELP_MESSAGE_MULTIMODALITY,
+    upload_files_to_gcs,
+)
+from utils.utils import save_chat
+
+EMPTY_CHAT_NAME = "Empty chat"
+NUM_CHAT_IN_RECENT = 3
+DEFAULT_BASE_URL = "http://localhost:8000/"
+
+
+class SideBar:
+    def __init__(self, st) -> None:
+        self.st = st
+
+    def init_side_bar(self):
+        with self.st.sidebar:
+            self.url_input_field = self.st.text_input(
+                label="Service URL",
+                value=os.environ.get("SERVICE_URL", DEFAULT_BASE_URL),
+            )
+            self.should_authenticate_request = self.st.checkbox(
+                label="Authenticate request",
+                value=False,
+                help="If checked, any request to the server will contain an"
+                "Identity token to allow authentication. "
+                "See the Cloud Run documentation to know more about authentication:"
+                "https://cloud.google.com/run/docs/authenticating/service-to-service",
+            )
+            col1, col2, col3 = self.st.columns(3)
+            with col1:
+                if self.st.button("+ New chat"):
+                    if (
+                        len(
+                            self.st.session_state.user_chats[
+                                self.st.session_state["session_id"]
+                            ]["messages"]
+                        )
+                        > 0
+                    ):
+                        self.st.session_state.run_id = None
+
+                        self.st.session_state["session_id"] = str(uuid.uuid4())
+                        self.st.session_state.session_db.get_session(
+                            session_id=self.st.session_state["session_id"],
+                        )
+                        self.st.session_state.user_chats[
+                            self.st.session_state["session_id"]
+                        ] = {
+                            "title": EMPTY_CHAT_NAME,
+                            "messages": [],
+                        }
+
+            with col2:
+                if self.st.button("Delete chat"):
+                    self.st.session_state.run_id = None
+                    self.st.session_state.session_db.clear()
+                    self.st.session_state.user_chats.pop(
+                        self.st.session_state["session_id"]
+                    )
+                    if len(self.st.session_state.user_chats) > 0:
+                        chat_id = list(self.st.session_state.user_chats.keys())[0]
+                        self.st.session_state["session_id"] = chat_id
+                        self.st.session_state.session_db.get_session(
+                            session_id=self.st.session_state["session_id"],
+                        )
+                    else:
+                        self.st.session_state["session_id"] = str(uuid.uuid4())
+                        self.st.session_state.user_chats[
+                            self.st.session_state["session_id"]
+                        ] = {
+                            "title": EMPTY_CHAT_NAME,
+                            "messages": [],
+                        }
+            with col3:
+                if self.st.button("Save chat"):
+                    save_chat(self.st)
+
+            self.st.subheader("Recent")  # Style the heading
+
+            all_chats = list(reversed(self.st.session_state.user_chats.items()))
+            for chat_id, chat in all_chats[:NUM_CHAT_IN_RECENT]:
+                if self.st.button(chat["title"], key=chat_id):
+                    self.st.session_state.run_id = None
+                    self.st.session_state["session_id"] = chat_id
+                    self.st.session_state.session_db.get_session(
+                        session_id=self.st.session_state["session_id"],
+                    )
+
+            with self.st.expander("Other chats"):
+                for chat_id, chat in all_chats[NUM_CHAT_IN_RECENT:]:
+                    if self.st.button(chat["title"], key=chat_id):
+                        self.st.session_state.run_id = None
+                        self.st.session_state["session_id"] = chat_id
+                        self.st.session_state.session_db.get_session(
+                            session_id=self.st.session_state["session_id"],
+                        )
+
+            self.st.divider()
+            self.st.header("Upload files from local")
+            bucket_name = self.st.text_input(
+                label="GCS Bucket for upload",
+                value=os.environ.get("BUCKET_NAME", "gs://your-bucket-name"),
+            )
+            if "checkbox_state" not in self.st.session_state:
+                self.st.session_state.checkbox_state = True
+
+            self.st.session_state.checkbox_state = self.st.checkbox(
+                "Upload to GCS first (suggested)", value=True, help=HELP_GCS_CHECKBOX
+            )
+
+            self.uploaded_files = self.st.file_uploader(
+                label="Send files from local",
+                accept_multiple_files=True,
+                key=f"uploader_images_{self.st.session_state.uploader_key}",
+                type=[
+                    "png",
+                    "jpg",
+                    "jpeg",
+                    "txt",
+                    "docx",
+                    "pdf",
+                    "rtf",
+                    "csv",
+                    "tsv",
+                    "xlsx",
+                ],
+            )
+            if self.uploaded_files and self.st.session_state.checkbox_state:
+                upload_files_to_gcs(self.st, bucket_name, self.uploaded_files)
+
+            self.st.divider()
+
+            self.st.header("Upload files from GCS")
+            self.gcs_uris = self.st.text_area(
+                "GCS uris (comma-separated)",
+                value=self.st.session_state["gcs_uris_to_be_sent"],
+                key=f"upload_text_area_{self.st.session_state.uploader_key}",
+                help=HELP_MESSAGE_MULTIMODALITY,
+            )
+
+            self.st.caption(f"Note: {HELP_MESSAGE_MULTIMODALITY}")

GoogleCloudPlatform/generative-ai

gemini/sample-apps/e2e-gen-ai-app-starter-kit/app/server.py
@@ -1,0 +1,115 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import os
+from typing import AsyncGenerator
+import uuid
+
+# ruff: noqa: I001
+from app.utils.input_types import Feedback, Input, InputChat, default_serialization
+from app.utils.output_types import EndEvent, Event
+from app.utils.tracing import CloudTraceLoggingSpanExporter
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse, StreamingResponse
+from google.cloud import logging as google_cloud_logging
+from traceloop.sdk import Instruments, Traceloop
+
+## Import the chain to be used
+from app.chain import chain
+
+# Or choose one of the following pattern chains to test by uncommenting it:
+
+# Custom RAG QA
+# from app.patterns.custom_rag_qa.chain import chain
+
+# LangGraph dummy agent
+# from app.patterns.langgraph_dummy_agent.chain import chain
+
+# The events that are supported by the UI Frontend
+SUPPORTED_EVENTS = [
+    "on_tool_start",
+    "on_tool_end",
+    "on_retriever_start",
+    "on_retriever_end",
+    "on_chat_model_stream",
+]
+
+# Initialize FastAPI app and logging
+app = FastAPI()
+logging_client = google_cloud_logging.Client()
+logger = logging_client.logger(__name__)
+
+# Initialize Traceloop
+try:
+    Traceloop.init(
+        app_name="Sample Chatbot Application",
+        disable_batch=False,
+        exporter=CloudTraceLoggingSpanExporter(),
+        instruments={Instruments.VERTEXAI, Instruments.LANGCHAIN},
+    )
+except Exception as e:
+    logging.error("Failed to initialize Traceloop: %s", e)
+
+
+async def stream_event_response(input_chat: InputChat) -> AsyncGenerator[str, None]:
+    run_id = uuid.uuid4()
+    input_dict = input_chat.model_dump()
+
+    Traceloop.set_association_properties(
+        {
+            "log_type": "tracing",
+            "run_id": str(run_id),
+            "user_id": input_dict["user_id"],
+            "session_id": input_dict["session_id"],
+            "commit_sha": os.environ.get("COMMIT_SHA", "None"),
+        }
+    )
+
+    yield json.dumps(
+        Event(event="metadata", data={"run_id": str(run_id)}),
+        default=default_serialization,
+    ) + "\n"
+
+    async for data in chain.astream_events(input_dict, version="v2"):
+        if data["event"] in SUPPORTED_EVENTS:
+            yield json.dumps(data, default=default_serialization) + "\n"
+
+    yield json.dumps(EndEvent(), default=default_serialization) + "\n"
+
+
+# Routes
+@app.get("/")
+async def redirect_root_to_docs() -> RedirectResponse:
+    return RedirectResponse("/docs")
+
+
+@app.post("/feedback")
+async def collect_feedback(feedback_dict: Feedback) -> None:
+    logger.log_struct(feedback_dict.model_dump(), severity="INFO")
+
+
+@app.post("/stream_events")
+async def stream_chat_events(request: Input) -> StreamingResponse:
+    return StreamingResponse(
+        stream_event_response(input_chat=request.input), media_type="text/event-stream"
+    )
+
+
+# Main execution
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)

gemini/sample-apps/e2e-gen-ai-app-starter-kit/deployment/terraform/apis.tf
@@ -1,0 +1,49 @@
+locals {
+  cicd_services = [
+    "cloudbuild.googleapis.com",
+    "discoveryengine.googleapis.com",
+    "aiplatform.googleapis.com",
+    "serviceusage.googleapis.com",
+    "bigquery.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "cloudtrace.googleapis.com"
+  ]
+
+  shared_services = [
+    "aiplatform.googleapis.com",
+    "run.googleapis.com",
+    "discoveryengine.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "bigquery.googleapis.com",
+    "serviceusage.googleapis.com",
+    "logging.googleapis.com",
+    "cloudtrace.googleapis.com"
+  ]
+
+  projects = {
+    prod    = var.prod_project_id
+    staging = var.staging_project_id
+  }
+
+}
+
+resource "google_project_service" "cicd_services" {
+  count              = length(local.cicd_services)
+  project            = var.cicd_runner_project_id
+  service            = local.cicd_services[count.index]
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "shared_services" {
+  for_each = {
+    for pair in setproduct(keys(local.projects), local.shared_services) :
+    "${pair[0]}_${replace(pair[1], ".", "_")}" => {
+      project = local.projects[pair[0]]
+      service = pair[1]
+    }
+  }
+  project            = each.value.project
+  service            = each.value.service
+  disable_on_destroy = false
+}

gemini/sample-apps/e2e-gen-ai-app-starter-kit/streamlit/side_bar.py
@@ -1,0 +1,161 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import uuid
+from typing import Any
+
+from utils.chat_utils import save_chat
+from utils.multimodal_utils import (
+    HELP_GCS_CHECKBOX,
+    HELP_MESSAGE_MULTIMODALITY,
+    upload_files_to_gcs,
+)
+
+EMPTY_CHAT_NAME = "Empty chat"
+NUM_CHAT_IN_RECENT = 3
+DEFAULT_BASE_URL = "http://localhost:8000/"
+
+
+class SideBar:
+    def __init__(self, st: Any) -> None:
+        self.st = st
+
+    def init_side_bar(self) -> None:
+        with self.st.sidebar:
+            self.url_input_field = self.st.text_input(
+                label="Service URL",
+                value=os.environ.get("SERVICE_URL", DEFAULT_BASE_URL),
+            )
+            self.should_authenticate_request = self.st.checkbox(
+                label="Authenticate request",
+                value=False,
+                help="If checked, any request to the server will contain an"
+                "Identity token to allow authentication. "
+                "See the Cloud Run documentation to know more about authentication:"
+                "https://cloud.google.com/run/docs/authenticating/service-to-service",
+            )
+            col1, col2, col3 = self.st.columns(3)
+            with col1:
+                if self.st.button("+ New chat"):
+                    if (
+                        len(
+                            self.st.session_state.user_chats[
+                                self.st.session_state["session_id"]
+                            ]["messages"]
+                        )
+                        > 0
+                    ):
+                        self.st.session_state.run_id = None
+
+                        self.st.session_state["session_id"] = str(uuid.uuid4())
+                        self.st.session_state.session_db.get_session(
+                            session_id=self.st.session_state["session_id"],
+                        )
+                        self.st.session_state.user_chats[
+                            self.st.session_state["session_id"]
+                        ] = {
+                            "title": EMPTY_CHAT_NAME,
+                            "messages": [],
+                        }
+
+            with col2:
+                if self.st.button("Delete chat"):
+                    self.st.session_state.run_id = None
+                    self.st.session_state.session_db.clear()
+                    self.st.session_state.user_chats.pop(
+                        self.st.session_state["session_id"]
+                    )
+                    if len(self.st.session_state.user_chats) > 0:
+                        chat_id = list(self.st.session_state.user_chats.keys())[0]
+                        self.st.session_state["session_id"] = chat_id
+                        self.st.session_state.session_db.get_session(
+                            session_id=self.st.session_state["session_id"],
+                        )
+                    else:
+                        self.st.session_state["session_id"] = str(uuid.uuid4())
+                        self.st.session_state.user_chats[
+                            self.st.session_state["session_id"]
+                        ] = {
+                            "title": EMPTY_CHAT_NAME,
+                            "messages": [],
+                        }
+            with col3:
+                if self.st.button("Save chat"):
+                    save_chat(self.st)
+
+            self.st.subheader("Recent")  # Style the heading
+
+            all_chats = list(reversed(self.st.session_state.user_chats.items()))
+            for chat_id, chat in all_chats[:NUM_CHAT_IN_RECENT]:
+                if self.st.button(chat["title"], key=chat_id):
+                    self.st.session_state.run_id = None
+                    self.st.session_state["session_id"] = chat_id
+                    self.st.session_state.session_db.get_session(
+                        session_id=self.st.session_state["session_id"],
+                    )
+
+            with self.st.expander("Other chats"):
+                for chat_id, chat in all_chats[NUM_CHAT_IN_RECENT:]:
+                    if self.st.button(chat["title"], key=chat_id):
+                        self.st.session_state.run_id = None
+                        self.st.session_state["session_id"] = chat_id
+                        self.st.session_state.session_db.get_session(
+                            session_id=self.st.session_state["session_id"],
+                        )
+
+            self.st.divider()
+            self.st.header("Upload files from local")
+            bucket_name = self.st.text_input(
+                label="GCS Bucket for upload",
+                value=os.environ.get("BUCKET_NAME", "gs://your-bucket-name"),
+            )
+            if "checkbox_state" not in self.st.session_state:
+                self.st.session_state.checkbox_state = True
+
+            self.st.session_state.checkbox_state = self.st.checkbox(
+                "Upload to GCS first (suggested)", value=False, help=HELP_GCS_CHECKBOX
+            )
+
+            self.uploaded_files = self.st.file_uploader(
+                label="Send files from local",
+                accept_multiple_files=True,
+                key=f"uploader_images_{self.st.session_state.uploader_key}",
+                type=[
+                    "png",
+                    "jpg",
+                    "jpeg",
+                    "txt",
+                    "docx",
+                    "pdf",
+                    "rtf",
+                    "csv",
+                    "tsv",
+                    "xlsx",
+                ],
+            )
+            if self.uploaded_files and self.st.session_state.checkbox_state:
+                upload_files_to_gcs(self.st, bucket_name, self.uploaded_files)
+
+            self.st.divider()
+
+            self.st.header("Upload files from GCS")
+            self.gcs_uris = self.st.text_area(
+                "GCS uris (comma-separated)",
+                value=self.st.session_state["gcs_uris_to_be_sent"],
+                key=f"upload_text_area_{self.st.session_state.uploader_key}",
+                help=HELP_MESSAGE_MULTIMODALITY,
+            )
+
+            self.st.caption(f"Note: {HELP_MESSAGE_MULTIMODALITY}")