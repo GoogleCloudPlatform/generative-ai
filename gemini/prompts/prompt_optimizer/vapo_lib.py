b1ab38019a833f6ede18b380fbb82094eb8208f1
inardini <inardini@google.com>
2024-11-18 00:31:28 +0000

    remove model function

--- gemini/prompts/prompt_optimizer/vapo_lib.py
+++ gemini/prompts/prompt_optimizer/vapo_lib.py
@@ -20,7 +20,7 @@ import random
 import re
 import string
 import subprocess
-from typing import Any, Dict, List, Optional, Union
+from typing import Dict, List, Optional, Union
 
 from IPython.core.display import DisplayHandle
 from IPython.display import HTML, display
@@ -32,9 +32,7 @@ import pandas as pd
 import plotly.graph_objects as go
 from tenacity import retry, wait_random_exponential
 from tensorflow.io import gfile
-from vertexai import generative_models
 from vertexai.evaluation import EvalTask
-from vertexai.generative_models import GenerationConfig, GenerativeModel
 
 
 def is_target_required_metric(eval_metric: str) -> bool:
@@ -772,64 +770,6 @@ def print_df_rows(
 
         if index + 1 >= n:
             break
-
-
-def init_new_model(
-    model_name: str,
-    generation_config: Optional[
-        Union[
-            generative_models._generative_models.GenerationConfig,
-            Dict[str, Any],
-        ]
-    ] = None,
-    safety_settings: Optional[
-        Union[
-            List[generative_models._generative_models.SafetySetting],
-            Dict[
-                aiplatform_v1beta1.types.content.HarmCategory,
-                aiplatform_v1beta1.types.content.SafetySetting.HarmBlockThreshold,
-            ],
-        ]
-    ] = None,
-) -> GenerativeModel:
-    """Initialize a new model with configurable generation and safety settings."""
-
-    # Use default configurations if none are provided
-    if generation_config is None:
-        generation_config = GenerationConfig(
-            candidate_count=1, max_output_tokens=2048, temperature=0.5
-        )
-    if not safety_settings:
-        safety_settings = [
-            generative_models.SafetySetting(
-                category=generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-                method=generative_models.HarmBlockMethod.SEVERITY,
-                threshold=generative_models.HarmBlockThreshold.BLOCK_NONE,
-            ),
-            generative_models.SafetySetting(
-                category=generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-                method=generative_models.HarmBlockMethod.SEVERITY,
-                threshold=generative_models.HarmBlockThreshold.BLOCK_NONE,
-            ),
-            generative_models.SafetySetting(
-                category=generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-                method=generative_models.HarmBlockMethod.SEVERITY,
-                threshold=generative_models.HarmBlockThreshold.BLOCK_NONE,
-            ),
-            generative_models.SafetySetting(
-                category=generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT,
-                method=generative_models.HarmBlockMethod.SEVERITY,
-                threshold=generative_models.HarmBlockThreshold.BLOCK_NONE,
-            ),
-        ]
-
-    # Initialize the model
-    model = GenerativeModel(
-        model_name=model_name,
-        generation_config=generation_config,
-        safety_settings=safety_settings,
-    )
-    return model
 
 
 def plot_eval_metrics(
