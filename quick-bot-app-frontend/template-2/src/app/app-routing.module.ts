0b77a945c7d25e6139be0456f88ab74c6a21533a
chaudharip <chaudharip@google.com>
2024-11-27 09:03:17 +0000

    creation of speerate folders for templates

---
+++ quick-bot-app-frontend/template-2/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './components/main/chat/chat.component';
+import { MainComponent } from './components/main/main.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './services/login/auth.guard';
+import { ManageIntentComponent } from './components/manage-intent/manage-intent.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginComponent},
+  {path: 'chat', component: ChatComponent, canActivate: [AuthGuard]},
+  {path: 'intent-management', component: ManageIntentComponent, canActivate:[AuthGuard]}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ quick-bot-app-frontend/template-2/src/app/components/main/chat/chat.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatComponent } from './chat.component';
+
+describe('ChatComponent', () => {
+  let component: ChatComponent;
+  let fixture: ComponentFixture<ChatComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ChatComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ChatComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
