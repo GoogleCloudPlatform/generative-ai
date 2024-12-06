9dfe2c3880535dd3000dcc096fbace68126cafd7
Ivan Rufino <ivanerufino@google.com>
2024-12-05 21:04:05 -0300

    fix: cleaning environment

--- template-2/frontend/src/environments/environment.ts
+++ template-2/frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     authDomain: "",
     projectId: "",
   },
-  requiredLogin: "False",
-  backendURL: "https://eren-backend-581595877765.us-central1.run.app/api",
-  chatbotName: "Eren",
+  requiredLogin: "",
+  backendURL: "",
+  chatbotName: "",
 };
