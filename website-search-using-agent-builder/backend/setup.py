7c66248adfb7e175f391b9ea37d39f0289d3b073
dineshvaryani <dineshvaryani@google.com>
2024-12-11 16:01:28 +0530

    Changes for bigquery agent_config table

--- website-search-using-agent-builder/backend/setup.py
+++ website-search-using-agent-builder/backend/setup.py
@@ -1,9 +1,10 @@
-from scripts.big_query_setup import create_dataset
+from scripts.big_query_setup import create_dataset, create_table
 
 BIG_QUERY_DATASET=""
 
 print("Setting up BigQuery... \n")
 
 create_dataset(BIG_QUERY_DATASET)
+create_table(BIG_QUERY_DATASET, AGENT_CONFIG_TABLE, AGENT_CONFIG.__schema__())
 
 print("\nSuccess!\n")
---
+++ website-search-using-agent-builder/backend/src/model/agent_config.py
@@ -0,0 +1,54 @@
+from google.cloud.bigquery import SchemaField
+from pydantic import BaseModel
+from typing import List
+
+
+class AgentConfig(BaseModel):
+    name: str
+    url: str
+
+    def __schema__() -> List[SchemaField]:
+        return [
+            SchemaField("name", "STRING", mode="REQUIRED"),
+            SchemaField("url", "STRING", mode="REQUIRED")
+        ]
+    
+    def __from_row__(row):
+        return AgentConfig(
+            name=row[0],
+            url=row[1]
+        )
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "url": self.url,
+        }
+    
+    def to_insert_string(self):
+        return f'"{self.name}", "{self.url}"'
+    
+    def get_standard_name(self) -> str:
+        return self.name.lower().replace(" ", "-").replace("_", "-")
+
+class CreateAgentConfigRequest(BaseModel):
+    name: str
+    url: str
+
+    def __schema__() -> List[SchemaField]:
+        return [
+            SchemaField("name", "STRING", mode="REQUIRED"),
+            SchemaField("url", "STRING", mode="REQUIRED"),
+        ]
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "url": self.url,
+        }
+    
+    def to_intent(self) -> AgentConfig:
+        return AgentConfig(
+            name=self.name,
+            url=self.url,
+        )
