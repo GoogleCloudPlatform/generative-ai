d68e34e24cd3e1af52555e9314492770e3b1febe
Holt Skinner <holtskinner@google.com>
2024-12-18 10:33:28 -0600

    Formatting

--- gemini/sample-apps/swot-agent/main.py
+++ gemini/sample-apps/swot-agent/main.py
@@ -1,5 +1,5 @@
 # Copyright 2024 Google, LLC.
-# This software is provided as-is, without warranty 
+# This software is provided as-is, without warranty
 # or representation for any use or purpose.
 # Your use of it is subject to your agreement with Google.
 
