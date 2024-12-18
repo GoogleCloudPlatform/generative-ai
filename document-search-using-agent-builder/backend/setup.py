3151ad9f88b5ba21c481f00982f4241abc2f960b
dineshvaryani <dineshvaryani@google.com>
2024-12-18 12:56:13 +0530

    Changes for the bigquery config

--- document-search-using-agent-builder/backend/setup.py
+++ document-search-using-agent-builder/backend/setup.py
@@ -1,12 +1,12 @@
 from scripts.big_query_setup import create_dataset, create_table
-from src.service.agent_config import AGENT_CONFIG_TABLE
-from src.model.agent_config import AgentConfig
+from src.service.search_application import SEARCH_APPLICATION_TABLE
+from src.model.search import SearchApplication
 
 BIG_QUERY_DATASET=""
 
 print("Setting up BigQuery... \n")
 
 create_dataset(BIG_QUERY_DATASET)
-create_table(BIG_QUERY_DATASET, AGENT_CONFIG_TABLE, AgentConfig.__schema__())
+create_table(BIG_QUERY_DATASET, SEARCH_APPLICATION_TABLE, SearchApplication.__schema__())
 
 print("\nSuccess!\n")
