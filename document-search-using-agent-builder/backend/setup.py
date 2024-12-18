00ff432284763d4ec0c7d5d67de4e1a9b41dc6d5
dineshvaryani <dineshvaryani@google.com>
2024-12-18 18:08:38 +0530

    Changes for the cors

--- document-search-using-agent-builder/backend/setup.py
+++ document-search-using-agent-builder/backend/setup.py
@@ -1,17 +1,8 @@
 from scripts.big_query_setup import create_dataset, create_table
-from scripts.gcs_setup import create_bucket, BUCKET
 from src.service.search_application import SEARCH_APPLICATION_TABLE
 from src.model.search import SearchApplication
 
 BIG_QUERY_DATASET=""
-
-
-print("Setting up GCS... \n")
-
-bucket = con(BUCKET)
-
-print("\nSuccess!\n")
-
 
 print("Setting up BigQuery... \n")
 
