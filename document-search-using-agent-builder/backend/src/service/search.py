98299ff4418b7ff4a8bce6e182c3e3ce2e151a7e
dineshvaryani <dineshvaryani@google.com>
2024-12-20 14:39:46 +0530

    Changes for search response

--- document-search-using-agent-builder/backend/src/service/search.py
+++ document-search-using-agent-builder/backend/src/service/search.py
@@ -1,7 +1,7 @@
 import requests
 from typing import List
 from typing import List
-from google.cloud.discoveryengine_v1 import SearchRequest, SearchServiceClient
+from google.cloud.discoveryengine_v1 import SearchRequest, SearchServiceClient, SearchResponse
 from src.model.search import SearchApplication, SearchResult
 
 CONTENT_SEARCH_SPEC = SearchRequest.ContentSearchSpec(
@@ -36,7 +36,7 @@ class SearchService:
         )
         self.serving_config = search_application.get_serving_config()
 
-    def search(self, term: str) -> List[SearchResult]:
+    def search(self, term: str) -> List[SearchResponse]:
         request = SearchRequest(
             serving_config=self.serving_config,
             query=term,
@@ -52,24 +52,24 @@ class SearchService:
         print(data)
 
         # Process results
-        for r in data.results:
-            document = r.document
-            derived_data = document.derived_struct_data
-            gcs_link = derived_data.get("link").replace("gs://", "https://storage.cloud.google.com/")
+        # for r in data.results:
+        #     document = r.document
+        #     derived_data = document.derived_struct_data
+        #     gcs_link = derived_data.get("link").replace("gs://", "https://storage.cloud.google.com/")
 
-            # Extract snippet safely
-            snippets = derived_data.get("snippets", [])
-            snippet_text = snippets[0].get("snippet", "No snippet available") if snippets else "No snippet available"
-            extractive_answers = derived_data.get("extractive_answers", [])
-            content_text = extractive_answers[0].get("content", "No content available") if extractive_answers else "No content available"
-            # Map to SearchResult
-            mapped_result = SearchResult(
-                document_id=document.id,
-                title=derived_data.get("title", "Untitled"),
-                snippet=snippet_text,
-                link=gcs_link,
-                content=content_text
-            )
-            results.append(mapped_result)
+        #     # Extract snippet safely
+        #     snippets = derived_data.get("snippets", [])
+        #     snippet_text = snippets[0].get("snippet", "No snippet available") if snippets else "No snippet available"
+        #     extractive_answers = derived_data.get("extractive_answers", [])
+        #     content_text = extractive_answers[0].get("content", "No content available") if extractive_answers else "No content available"
+        #     # Map to SearchResult
+        #     mapped_result = SearchResult(
+        #         document_id=document.id,
+        #         title=derived_data.get("title", "Untitled"),
+        #         snippet=snippet_text,
+        #         link=gcs_link,
+        #         content=content_text
+        #     )
+        #     results.append(mapped_result)
 
-        return results
+        return data
