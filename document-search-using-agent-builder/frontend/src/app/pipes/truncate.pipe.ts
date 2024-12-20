944dd68bcb72798f1102d1e088e5ca8533617c69
chaudharip <chaudharip@google.com>
2024-12-20 10:32:10 +0000

    show more content on cards

--- document-search-using-agent-builder/frontend/src/app/pipes/truncate.pipe.ts
+++ document-search-using-agent-builder/frontend/src/app/pipes/truncate.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
 
-  transform(value: string, limit = 100, trail = '...'): string {
+  transform(value: string, limit = 300, trail = '...'): string {
     return value.length > limit ? value.substring(0, limit) + trail : value;
   }
 
