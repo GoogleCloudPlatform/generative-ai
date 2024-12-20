3193ec05a187a157162e22472c7ceb80c1c757c0
Heiko Hotz <heikohotz+git@gmail.com>
2024-12-20 14:50:58 +0000

    style: Format PCM processor with Prettier

--- gemini/multimodal-live-api/websocket-demo-app/frontend/pcm-processor.js
+++ gemini/multimodal-live-api/websocket-demo-app/frontend/pcm-processor.js
@@ -4,31 +4,31 @@
  * @description Processes PCM audio data in a Web Audio API context
  */
 class PCMProcessor extends AudioWorkletProcessor {
-    constructor() {
-        super();
-        this.buffer = new Float32Array();
+  constructor() {
+    super();
+    this.buffer = new Float32Array();
 
-        this.port.onmessage = (e) => {
-            const newData = e.data;
-            const newBuffer = new Float32Array(this.buffer.length + newData.length);
-            newBuffer.set(this.buffer);
-            newBuffer.set(newData, this.buffer.length);
-            this.buffer = newBuffer;
-        };
+    this.port.onmessage = (e) => {
+      const newData = e.data;
+      const newBuffer = new Float32Array(this.buffer.length + newData.length);
+      newBuffer.set(this.buffer);
+      newBuffer.set(newData, this.buffer.length);
+      this.buffer = newBuffer;
+    };
+  }
+
+  process(inputs, outputs, parameters) {
+    const output = outputs[0];
+    const channelData = output[0];
+
+    if (this.buffer.length >= channelData.length) {
+      channelData.set(this.buffer.slice(0, channelData.length));
+      this.buffer = this.buffer.slice(channelData.length);
+      return true;
     }
 
-    process(inputs, outputs, parameters) {
-        const output = outputs[0];
-        const channelData = output[0];
-
-        if (this.buffer.length >= channelData.length) {
-            channelData.set(this.buffer.slice(0, channelData.length));
-            this.buffer = this.buffer.slice(channelData.length);
-            return true;
-        }
-
-        return true;
-    }
+    return true;
+  }
 }
 
-registerProcessor('pcm-processor', PCMProcessor);
+registerProcessor("pcm-processor", PCMProcessor);
