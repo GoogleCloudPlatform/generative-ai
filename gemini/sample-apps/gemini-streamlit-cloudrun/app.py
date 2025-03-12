8e725648870f02fa07d8a29149408f20b56114d2
Holt Skinner <13262395+holtskinner@users.noreply.github.com>
2025-03-12 10:16:30 -0500

    chore: Remove Gemini 1.5 from Streamlit App (#1823)

--- gemini/sample-apps/gemini-streamlit-cloudrun/app.py
+++ gemini/sample-apps/gemini-streamlit-cloudrun/app.py
@@ -43,8 +43,6 @@ MODELS = {
     "gemini-2.0-flash-lite": "Gemini 2.0 Flash-Lite",
     "gemini-2.0-flash-thinking-exp-01-21": "Gemini 2.0 Flash Thinking",
     "gemini-2.0-pro-exp-02-05": "Gemini 2.0 Pro",
-    "gemini-1.5-flash": "Gemini 1.5 Flash",
-    "gemini-1.5-pro": "Gemini 1.5 Pro",
 }
 
 
