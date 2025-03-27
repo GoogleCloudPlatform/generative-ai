00c53b52100cb221b838bcae21949ae8bb71dec3
Piyush Sarraf <piyushsarraf@google.com>
2025-03-27 05:21:24 +0000

    refactor: Adding tailwind CSS support to text-to-imagen

--- text-to-image-using-imagen3/frontend/src/environments/environment.ts
+++ text-to-image-using-imagen3/frontend/src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     measurementId: "<your measurementId>"
   },
   requiredLogin: 'False',
-  backendURL: 'http://localhost:8080/api',
-  chatbotName: 'My New Agent',
+  backendURL: 'https://quickbot-text-to-imagen-agent-backend-596315537962.southamerica-east1.run.app/api',
+  chatbotName: 'Test 1',
 };
