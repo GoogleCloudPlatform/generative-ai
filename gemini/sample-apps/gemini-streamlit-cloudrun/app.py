dfab0dd050c6110cc4b594e71938dd2691d2aef1
Holt Skinner <holtskinner@google.com>
2025-05-06 14:22:15 -0500

    chore: Update Gemini 2.5 Pro to use latest version

--- gemini/sample-apps/gemini-streamlit-cloudrun/app.py
+++ gemini/sample-apps/gemini-streamlit-cloudrun/app.py
@@ -38,7 +38,7 @@ def _region() -> str:
 MODELS = {
     "gemini-2.0-flash": "Gemini 2.0 Flash",
     "gemini-2.0-flash-lite": "Gemini 2.0 Flash-Lite",
-    "gemini-2.5-pro-preview-03-25": "Gemini 2.5 Pro",
+    "gemini-2.5-pro-preview-05-06": "Gemini 2.5 Pro",
     "gemini-2.5-flash-preview-04-17": "Gemini 2.5 Flash",
     "model-optimizer-exp-04-09": "Model Optimizer",
 }
