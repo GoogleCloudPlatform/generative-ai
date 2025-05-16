e86a76adb653d04685521bf3be43cf9e3bf30a08
CadillacBurgess1 <140216796+CadillacBurgess1@users.noreply.github.com>
2025-05-16 14:16:02 -0400

    ci: Update Notebook testing framework to add L1 notebooks (#2062)

--- .cloud-build/executor/test_notebooks.sh
+++ .cloud-build/executor/test_notebooks.sh
@@ -30,7 +30,7 @@ for x in $TARGET; do
   OPERATION_ID=$(gcloud colab executions create \
     --display-name="$DISPLAY_NAME" \
     --notebook-runtime-template="$NOTEBOOK_RUNTIME_TEMPLATE" \
-    --gcs-notebook-uri="$OUTPUT_URI/generative-ai/gemini/getting-started/${x##*/}" \
+    --gcs-notebook-uri="$OUTPUT_URI/$x" \
     --gcs-output-uri="$OUTPUT_URI" \
     --project="$PROJECT_ID" \
     --region="$REGION" \
