2cd180a24ceb9ecb82241b07e5cf2a8327cd00a0
EmanuelB25 <emanuelburgess@google.com>
2025-05-16 12:41:19 -0400

    Revert "update to add L1 notebooks"
    This reverts commit 6f44ad1384d4050b5d3275cedcef65fc271f7094.

--- .cloud-build/clone-repo/clone_repo.sh
+++ .cloud-build/clone-repo/clone_repo.sh
@@ -4,10 +4,10 @@
 OUTPUT_URI=$(cat OUTPUT_URI)
 
 # Clones the generative-ai repository from GitHub.
-git clone --depth 1 -b main https://github.com/GoogleCloudPlatform/generative-ai.git
+git clone https://github.com/GoogleCloudPlatform/generative-ai.git
 
 # Changes the current directory to the cloned repository.
 cd generative-ai || exit 1
 
 # Copies the specified directory to the destination GCS URI using gsutil.
-gsutil -m rsync -r . $OUTPUT_URI/generative-ai/
+gsutil -m cp -r . $OUTPUT_URI/generative-ai/gemini/getting-started/
