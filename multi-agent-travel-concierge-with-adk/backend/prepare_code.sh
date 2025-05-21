253608a97bf3ead0788733f5e2628ba7b5e66990
Mauro Cominotti <maurocominotti@google.com>
2025-05-21 09:53:14 -0300

    fix: update prepare_code.sh to use profiles folder instead of eval

--- multi-agent-travel-concierge-with-adk/backend/prepare_code.sh
+++ multi-agent-travel-concierge-with-adk/backend/prepare_code.sh
@@ -5,7 +5,7 @@ echo "Starting prepare_code.sh script..."
 # https://github.com/google/adk-samples.git/adk-samples/agents/travel-concierge/travel_concierge # TODO: Pass this as a template parameter to generalize solution for any ADK agent
 git clone https://github.com/google/adk-samples.git
 cp -r adk-samples/agents/travel-concierge/travel_concierge ./travel_concierge
-cp -r adk-samples/agents/travel-concierge/eval ./eval
+cp -r adk-samples/agents/travel-concierge/profiles ./profiles
 cp adk-samples/agents/travel-concierge/pyproject.toml . # TODO: Installing dependencies might be necessary only for local and not AE deployment
 # cp adk-samples/agents/travel-concierge/poetry.lock . # TODO: Double check this, poetry.lock does not exist on the ADK repo
 poetry install --with deployment
