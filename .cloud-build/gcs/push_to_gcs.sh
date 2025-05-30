09c1540817cff4ad5ba5737fd7daedbee54a16f9
Holt Skinner <13262395+holtskinner@users.noreply.github.com>
2025-05-30 14:09:42 -0400

    ci: Updates to Cloud Build NB Testing Scripts (#2039)

--- .cloud-build/gcs/push_to_gcs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-OUTPUT_URI=$(cat OUTPUT_URI)
-
-for filename in /workspace/generative-ai/gemini/getting-started/*.ipynb; do
-  name=$(basename "$filename")
-  gcloud storage cp "$filename" $OUTPUT_URI/generative-ai/gemini/getting-started/"$name"
-done
